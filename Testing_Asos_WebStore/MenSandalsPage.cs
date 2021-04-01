2f2eb32b03388c2150dfc4d0453ff3d5e5144dbb
RAGE\Javor <javor.andr@gmail.com>
2021-04-01 14:37:35 +0300

    adding two more classes to men section

---
+++ Testing_Asos_WebStore/MenSandalsPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace Testing_Asos_WebStore
+{
+    internal class MenSandalsPage
+    {
+        private IWebDriver Driver;
+        private WebDriverWait wait;
+        public MenSandalsPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public bool IsLoaded 
+        {
+            get 
+            {
+                wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(7));
+                return wait.Until(ExpectedConditions.ElementExists(By.Id("category-banner-wrapper"))).Displayed;
+            }
+        }
+    }
+}
---
+++ Testing_Asos_WebStore/OnlyBlackSandals.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace Testing_Asos_WebStore
+{
+    internal class OnlyBlackSandals
+    {
+        private IWebDriver driver;
+        private WebDriverWait wait;
+        public OnlyBlackSandals(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public bool IsProperlyLoaded
+        {
+            get
+            {
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+                var drMartensSandals = wait.Until(ExpectedConditions.ElementExists(By.XPath("//p[contains(text(),'geraldo sandals in black')]"))).Displayed;
+                return drMartensSandals;
+            }
+        }
+    }
+}
