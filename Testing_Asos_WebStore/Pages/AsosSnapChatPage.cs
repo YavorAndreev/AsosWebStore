dbe146d3de92798576e59eb3a75bfc25f9d107ad
RAGE\Javor <javor.andr@gmail.com>
2021-04-11 17:53:23 +0300

    testing snapchat icon on asos page

---
+++ Testing_Asos_WebStore/Pages/AsosSnapChatPage.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using ExpectedConditions = SeleniumExtras.WaitHelpers.ExpectedConditions;
+
+namespace Testing_Asos_WebStore
+{
+    internal class AsosSnapChatPage
+    {
+        private IWebDriver Driver { get; }
+
+        public AsosSnapChatPage(IWebDriver driver)
+        {
+            Driver = driver;
+        }
+
+        public bool IsLoaded
+        {
+            get
+            {
+                try
+                {
+                    var wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(6));
+                    return wait.Until(ExpectedConditions.UrlContains("asosfashion"));
+                }
+                catch(WebDriverTimeoutException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return false;
+                }
+            }
+        }
+    }
+}
--- Testing_Asos_WebStore/Tests/WomenSectionTests.cs
+++ Testing_Asos_WebStore/Tests/WomenSectionTests.cs
@@ -28,5 +28,15 @@ namespace Testing_Asos_WebStore.Tests
 
             Assert.IsTrue(asosLuxePage.IsLoaded);
         }
+
+        [TestMethod]
+        public void TestAsosSnapChatPage()
+        {
+            HomePage.GoToUrl();
+            HomePage.ClickWomenLink();
+            var asosSnapChatPage = HomePage.WomenSection.ClickSnapChatIcon();
+
+            Assert.IsTrue(asosSnapChatPage.IsLoaded, "You are on a wrong webpage");
+        }
     }
 }
